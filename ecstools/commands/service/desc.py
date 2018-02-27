62c5298d5e8d7604653e54d5471fdcac150733a7
Borislav Ivanov <borogl@gmail.com>
2018-02-26 21:37:09 -0800

    Add service describe command

---
+++ ecstools/commands/service/desc.py
@@ -0,0 +1,65 @@
+import click
+
+
+@click.command()
+@click.argument('cluster')
+@click.argument('service')
+@click.pass_context
+def cli(ctx, cluster, service):
+    """Describe service"""
+    ecs = ctx.obj['ecs']
+    elbv2 = ctx.obj['elbv2']
+
+    response = ecs.describe_services(
+        cluster=cluster,
+        services=[service]
+    )
+    services = response['services']
+
+    for s in services:
+
+        # Print Service Info
+        cls_name = s['clusterArn'].split('/')[-1]
+        def_name = s['taskDefinition'].split('/')[-1]
+        general_info = (cls_name, s['serviceName'], def_name, s['launchType'])
+        counts = (s['desiredCount'], s['runningCount'], s['pendingCount'])
+
+        click.secho('%s %s %s %s' % (general_info), fg='blue')
+        click.secho(('Desired: %s Running: %s Pending: %s' %
+                     (counts)), fg='white')
+        click.echo('Created: %s' % s['createdAt'].replace(microsecond=0))
+        click.echo()
+
+        click.echo(s['roleArn'])
+        click.echo()
+
+        for lb in s['loadBalancers']:
+            tgs_state = {}
+            if 'loadBalancerName' in lb:
+                click.echo('LoadBalancer: %s' % lb['loadBalancerName'])
+            if 'targetGroupArn' in lb:
+                click.echo('Target Group: %s' %
+                           lb['targetGroupArn'].split('/')[-2], nl=False)
+                targets = elbv2.describe_target_health(
+                    TargetGroupArn=lb['targetGroupArn'])
+
+                for t in targets['TargetHealthDescriptions']:
+                    state = t['TargetHealth']['State']
+                    if state in tgs_state:
+                        tgs_state[state] += 1
+                    else:
+                        tgs_state[state] = 1
+            click.echo('  %s' % lb['containerName'], nl=False)
+            click.echo(' %s' % lb['containerPort'], nl=False)
+            if tgs_state:
+                states = ' '.join(['{}: {}'.format(k, v)
+                                   for k, v in tgs_state.items()])
+                click.echo('  %s' % states)
+            else:
+                click.echo()
+        click.echo()
+
+        nc = s['networkConfiguration']['awsvpcConfiguration']
+        click.echo('Subnets:          %s' % " ".join(nc['subnets']))
+        click.echo('Security Groups:  %s' % " ".join(nc['securityGroups']))
+        click.echo('Public IP:        %s' % nc['assignPublicIp'])
