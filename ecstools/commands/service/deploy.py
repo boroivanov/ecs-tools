168384079ed29be4a238ff4c3bacd8fec7a64a17
Borislav Ivanov <borogl@gmail.com>
2018-06-27 00:20:11 -0700

    Refactor funcs in deploy

--- ecstools/commands/service/deploy.py
+++ ecstools/commands/service/deploy.py
@@ -10,8 +10,10 @@ import ecstools.lib.utils as utils
 @click.argument('cluster')
 @click.argument('service')
 @click.argument('artifact')
-@click.option('-T', '--task-definition', is_flag=True, help='Deploy task definition')
-@click.option('-c', '--count', type=int, default=None, help='Update the current number of tasks')
+@click.option('-T', '--task-definition', is_flag=True,
+              help='Deploy task definition')
+@click.option('-c', '--count', type=int, default=None,
+              help='Update the current number of tasks')
 @click.pass_context
 def cli(ctx, cluster, service, artifact, task_definition, count):
     """Deploy a task definition to a service
@@ -44,32 +46,9 @@ def register_task_def_with_new_image(ecs, ecr, cluster, service, artifact):
     td = utils.describe_task_definition(ecs, td_arn)
     containers = td['containerDefinitions']
 
-    try:
-        ecr_repo, ecr_iamge_tag = containers[0]['image'].split(':')
-    except:
-        # If no tag was specified - defaulting to latest tag
-        ecr_repo = containers[0]['image']
+    ecr_repo, ecr_image_tag = get_repo_and_tag_names(containers)
 
-    # Check if image tag exist in the ECR repo
-    try:
-        ecr.describe_images(
-            repositoryName=ecr_repo.split('/')[-1],
-            imageIds=[
-                {
-                    'imageTag': artifact
-                },
-            ],
-            filter={
-                'tagStatus': 'TAGGED'
-            }
-        )
-    except ClientError as e:
-        if e.response['Error']['Code'] == 'ImageNotFoundException':
-            click.echo('Image not found: %s' % e, err=True)
-        else:
-            click.echo(e, err=True)
-        sys.exit(1)
-
+    verify_image_tag_in_ecr(ecr, ecr_repo, artifact)
     click.echo('Found image: %s:%s' % (ecr_repo, artifact))
 
     ###########################################################################
@@ -79,25 +58,16 @@ def register_task_def_with_new_image(ecs, ecr, cluster, service, artifact):
     # docker image (think tag:latest).
     # We skip registering a new task definition revision as it's not needed.
     ###########################################################################
-    if ecr_iamge_tag and ecr_iamge_tag == artifact:
-        click.secho(('%s:%s is already in the current task definition. ' % (
-            ecr_repo.split('/')[-1], ecr_iamge_tag)), nl=False, fg='white')
+    if image_tag_currently_deployed(ecr_image_tag, artifact):
+        click.secho(('%s:%s is already in the current task definition.' % (
+            ecr_repo.split('/')[-1], ecr_image_tag)), nl=False, fg='white')
         click.secho(('Forcing a new deployment of %s' %
                      td_arn.split('/')[-1]), fg='white')
         return td_arn
 
-    ###########################################################################
-    # Register new task definition with the new image
-    ###########################################################################
-    new_td = td.copy()
-    for k in ['status', 'compatibilities', 'taskDefinitionArn', 'revision', 'requiresAttributes']:
-        del new_td[k]
-    new_td['containerDefinitions'][0]['image'] = ':'.join([ecr_repo, artifact])
-    new_td_res = ecs.register_task_definition(**new_td)
-    td_name = new_td_res['taskDefinition']['taskDefinitionArn'].split('/')[-1]
-    click.secho('Registered new task definition: %s' % td_name, fg='green')
-
-    return td_name
+    new_td = copy_task_definition(td)
+    new_td_name = register_task_def(ecs, new_td, ecr_repo, artifact)
+    return new_td_name
 
 
 def deploy_task_definition(ecs, cluster, service, task_def, count):
@@ -113,5 +83,59 @@ def deploy_task_definition(ecs, cluster, service, task_def, count):
     if count:
         params['desiredCount'] = count
 
-    res = utils.update_service(ecs, **params)
-    return res
+    utils.update_service(ecs, **params)
+
+
+def register_task_def(ecs, td, ecr_repo, artifact):
+    td['containerDefinitions'][0]['image'] = ':'.join([ecr_repo, artifact])
+    new_td_res = ecs.register_task_definition(**td)
+    td_name = new_td_res['taskDefinition']['taskDefinitionArn'].split('/')[-1]
+    click.secho('Registered new task definition: %s' % td_name, fg='green')
+    return td_name
+
+
+def verify_image_tag_in_ecr(ecr, repo, tag):
+    try:
+        ecr.describe_images(
+            repositoryName=repo.split('/')[-1],
+            imageIds=[
+                {
+                    'imageTag': tag
+                },
+            ],
+            filter={
+                'tagStatus': 'TAGGED'
+            }
+        )
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'ImageNotFoundException':
+            click.echo('Image not found: %s' % e, err=True)
+        else:
+            click.echo(e, err=True)
+        sys.exit(1)
+
+
+def get_repo_and_tag_names(containers):
+    try:
+        repo, tag = containers[0]['image'].split(':')
+    except ValueError:
+        repo, tag = containers[0]['image'], 'latest'
+    return repo, tag
+
+
+def image_tag_currently_deployed(ecr_image_tag, artifact):
+    return ecr_image_tag == artifact
+
+
+def copy_task_definition(td):
+    aws_reserved_params = ['status',
+                           'compatibilities',
+                           'taskDefinitionArn',
+                           'revision',
+                           'requiresAttributes'
+                           ]
+    new_td = td.copy()
+
+    for k in aws_reserved_params:
+        del new_td[k]
+    return new_td
