cf47a1574210d62f9d7adb790522b74fc998423e
Borislav Ivanov <borogl@gmail.com>
2018-07-19 11:14:06 -0700

    Add more tests for aliased commands

--- ecstools/tests/commands/test_task_definition.py
+++ ecstools/tests/commands/test_task_definition.py
@@ -11,6 +11,11 @@ class TestTaskDefinition(object):
         expected = 'production-app1\nproduction-app2\nproduction-worker1\n'
         assert result.output == expected
 
+    def test_task_definition_ls_alias(self, runner):
+        result = runner.invoke(main.cli, ['td'])
+        expected = 'production-app1\nproduction-app2\nproduction-worker1\n'
+        assert result.output == expected
+
     # moto list_task_definitions filtering not implemented
     # def test_task_definition_ls_revisions(self, runner):
     #     result = runner.invoke(
