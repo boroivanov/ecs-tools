7d1ade74bb401beb9555fcbe8f0671ed2f3a30b1
Borislav Ivanov <borogl@gmail.com>
2019-03-25 23:15:54 -0700

    switch from reprint to curses

--- ecstools/lib/utils.py
+++ ecstools/lib/utils.py
@@ -1,6 +1,6 @@
 import sys
 import time
-from reprint import output
+import curses
 
 from ecstools.resources.service import Service
 
@@ -22,38 +22,47 @@ def monitor_deployment(ecs, elbv2, cluster, services, interval=5,
     if not isinstance(services, list):
         services = [services]
 
-    with output(initial_len=20, interval=0) as out:
+    scr = curses.initscr()
+    curses.noecho()
+    curses.cbreak()
+
+    try:
         while True:
             index = index_generator()
             gmt, elapsed = get_elapsed_time(start_time)
-            out[next(index)] = 'Elapsed: {}'.format(elapsed)
 
+            scr.addstr(next(index), 0, f'Elapsed: {elapsed}')
             if gmt.tm_sec % interval == 0:
-                print_deployment_info(index, out, ecs, elbv2, cluster,
+                scr.addstr(next(index), 0, '\n')
+                print_deployment_info(index, scr, ecs, elbv2, cluster,
                                       services, srv_len, exit_on_complete)
+            scr.refresh()
             time.sleep(1)
+    finally:
+        curses.echo()
+        curses.nocbreak()
+        curses.endwin()
 
 
-def print_deployment_info(index, out, ecs, elbv2, cluster, services,
+def print_deployment_info(index, scr, ecs, elbv2, cluster, services,
                           srv_len, exit_on_complete):
     statuses = {}
     for service in services:
         srv = Service(ecs, None, cluster, service)
         status = print_group_deployment_info(
-            index, out, ecs, elbv2, srv, srv_len)
+            index, scr, ecs, elbv2, srv, srv_len)
         statuses[service] = status
 
-    out[next(index)] = '\n'
-    out[next(index)] = 'Ctrl-C to quit the watcher.' \
-        ' No deployments will be interrupted.'
+    scr.addstr(next(index), 0, f'\nCtrl-C to quit the watcher.'
+               ' No deployments will be interrupted.')
 
-    if deployment_completed(index, out, statuses, exit_on_complete):
+    if deployment_completed(index, scr, statuses, exit_on_complete):
         sys.exit(0)
 
     del srv
 
 
-def print_group_deployment_info(index, out, ecs, elbv2, srv, srv_len):
+def print_group_deployment_info(index, scr, ecs, elbv2, srv, srv_len):
     for d in srv.deployments()[:1]:
         d_info = {
             'cluster': srv.cluster(),
@@ -69,7 +78,7 @@ def print_group_deployment_info(index, out, ecs, elbv2, srv, srv_len):
         d_info['status'] = deployment_status(srv, d)
         d_info, output_template = print_load_balancer_info(elbv2, srv, d_info,
                                                            output_template)
-        out[next(index)] = output_template.format(**d_info)
+        scr.addstr(next(index), 0, output_template.format(**d_info))
 
     return d_info['status']
 
@@ -132,10 +141,10 @@ def get_elapsed_time(start_time):
     return gmt, elapsed
 
 
-def deployment_completed(index, out, statuses, exit_on_complete):
+def deployment_completed(index, scr, statuses, exit_on_complete):
     if exit_on_complete:
         if all([x == 'Completed' for x in statuses.values()]):
-            out[next(index)] = 'All deployments completed.'
+            scr.addstr(next(index), 0, 'All deployments completed.')
             return True
     return False
 
